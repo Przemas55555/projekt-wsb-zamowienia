032a21d5edfa5cdccd61614c314518193c2f9e5f
Przemysław Kaczmarczyk <pk@nettle.pl>
2019-05-26 01:22:25 +0200

    reactive-product page

--- orders-app/src/main/java/com/pk/ordersapp/spring/controller/ProductController.java
+++ orders-app/src/main/java/com/pk/ordersapp/spring/controller/ProductController.java
@@ -6,13 +6,7 @@ import com.pk.ordersapp.spring.model.Product;
 import com.pk.ordersapp.spring.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ProductController {
@@ -35,6 +29,7 @@ public class ProductController {
    }
 
    /*---get all products---*/
+   @CrossOrigin(origins = "http://localhost:8081")
    @GetMapping("/product")
    public ResponseEntity<List<Product>> list() {
       List<Product> products = productService.list();
